14027ec5628d68e0f84ef5c17a54a259af72c318
Davi Borges <davi.borges@codificar.com.br>
2019-07-16 00:48:13 -0300

    Site Novo

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // ----------------------------------------------
+    // Preloader
+    // ----------------------------------------------
+    (function () {
+        $(window).load(function () {
+            $('#pre-status').fadeOut();
+            $('#st-preloader').delay(350).fadeOut('slow');
+        });
+    }());
+});    
